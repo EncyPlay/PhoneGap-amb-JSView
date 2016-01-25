6239739ae48fe07e80e77e1dae3692df60ae0d72
EncyPlay <pablo23082000@gmail.com>
2016-01-25 12:30:50 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,13 +3,37 @@
 */
 
 $JSView.controller = {
-    Inici: function(e){
+    inici: function(e){
 		$JSView.dataView({},e)
 	},
-    Sol: function(e){
+    sol: function(e){
 		$JSView.dataView({},e)	
 	},
-    Mercuri: function(e){
+    mercuri: function(e){
+		$JSView.dataView({},e)
+	},
+    jupiter: function(e){
+		$JSView.dataView({},e)
+	},
+    mart: function(e){
+		$JSView.dataView({},e)
+	},
+    neptu: function(e){
+		$JSView.dataView({},e)
+	},
+    saturn: function(e){
+		$JSView.dataView({},e)
+	},
+    taula: function(e){
+		$JSView.dataView({},e)
+	},
+    terra: function(e){
+		$JSView.dataView({},e)
+	},
+    ura: function(e){
+		$JSView.dataView({},e)
+	},
+    venus: function(e){
 		$JSView.dataView({},e)
 	},
     modalA: function(e){
